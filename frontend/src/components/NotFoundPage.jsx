bcc9685d349d445dcb880150dacb3316a0d0736a
Armine Palanjyan <a.p.t2@mail.ru>
2026-06-22 00:30:47 +0400

    update

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,8 +1,8 @@
-import { icon } from '../assets/404.svg';
+import icon from '../assets/404.svg';
 export default () => {
   return (
     <div className="text-center">
-      <img alt="Страница не найдена" className="img-fluid h-25" src={icon} />
+      <img alt="Страница не найдена" className="img-fluid h-25" src={icon} width="200"/>
       <h1 className="h4 text-muted">Страница не найдена</h1>
       <p className="text-muted">
         Но вы можете перейти <a href="/">на главную страницу</a>
