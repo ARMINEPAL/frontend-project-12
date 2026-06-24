eb34c0c6f1d32ad924b1ab0e325d6b85e300f51b
Armine Palanjyan <a.p.t2@mail.ru>
2026-06-24 17:04:47 +0400

    update

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -39,6 +39,7 @@ const MessageForm = ({ currentChannelId }) => {
           <input
             className="border-0 p-0 ps-2 form-control"
             name="body"
+            aria-label={t('chatPage.labels.forMessages')} 
             placeholder={t('chatPage.labels.forMessages')}
             value={text}
             onChange={(e) => setText(e.target.value)}
@@ -49,7 +50,22 @@ const MessageForm = ({ currentChannelId }) => {
             type="submit"
             disabled={!text.trim() || !currentChannelId}
           >
-            {t('buttons.send')}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="bi bi-arrow-right-square"
+            role="img"
+            aria-label={t('buttons.add')}
+          >
+          <path
+            fillRule="evenodd"
+            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+           />
+           </svg>
+  <span className="visually-hidden">{t('buttons.send')}</span>
           </button>
         </div>
       </form>
